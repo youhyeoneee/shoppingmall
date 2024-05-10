1d9a3b99d7c5e28ef173973e59d91c6035bf305c
youhyeoneee <khukyh2018@khu.ac.kr>
2024-05-10 10:19:01 +0900

    feat: add categoryId

--- src/main/java/com/example/shoppingmall/product/Product.java
+++ src/main/java/com/example/shoppingmall/product/Product.java
@@ -10,6 +10,7 @@ public class Product {
     private String name;
     private int price;
     private String description;
+    private int categoryId;
 
     // setter는 가능한 도메인 객체에는 만들면 안된다.
     // TODO setter는 DTO 등장하고. 지우러 오자!
