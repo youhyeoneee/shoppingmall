04e761b98b061a0efacf764665e33219c8f7c4e6
youhyeoneee <khukyh2018@khu.ac.kr>
2024-05-17 14:51:00 +0900

    feat: add valid to memberDTO

--- src/main/java/com/example/shoppingmall/member/MemberContoller.java
+++ src/main/java/com/example/shoppingmall/member/MemberContoller.java
@@ -1,10 +1,16 @@
 package com.example.shoppingmall.member;
 
 import com.example.shoppingmall.utils.ApiUtils;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.example.shoppingmall.utils.ApiUtils.error;
 import static com.example.shoppingmall.utils.ApiUtils.success;
@@ -16,7 +22,17 @@ public class MemberContoller {
     MemberService memberService;
 
     @PostMapping("/join")
-    public ApiUtils.ApiResult<String> join(@RequestBody MemberDTO memberDto) throws DuplicateException {
+    public ApiUtils.ApiResult<String> join(@Valid @RequestBody MemberDTO memberDto, Errors errors) {
+        if (errors.hasErrors()) {
+            String result = "[유효성 검사 실패]";
+            Map<String, String> validatorResult = new HashMap<>();
+            for (FieldError error : errors.getFieldErrors()) {
+                String validKeyName = error.getField();
+                validatorResult.put(validKeyName, error.getDefaultMessage());
+            }
+            return error(result + validatorResult, HttpStatus.BAD_REQUEST);
+        }
+
         if (isDuplicateId(memberDto)) {
             return error("아이디 중복", HttpStatus.CONFLICT);
         }
--- src/main/java/com/example/shoppingmall/member/MemberDTO.java
+++ src/main/java/com/example/shoppingmall/member/MemberDTO.java
@@ -1,7 +1,12 @@
 package com.example.shoppingmall.member;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 
@@ -9,10 +14,20 @@ import lombok.Setter;
 @Setter
 public class MemberDTO {
     @JsonProperty("user_id")
+    @NotBlank(message = "아이디를 입력해주세요.")
     String userId;
+
+    @Pattern(regexp = "\t^.*(?=^.{8,15}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$", message = "숫자, 문자, 특수문자 포함 8~15자리 이내여야 합니다.")
     String pw;
+
+    @NotBlank(message = "이름을 입력해주세요.")
     String name;
+
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "이메일 형식에 맞지 않습니다.")
     String email;
+
+    @Pattern(regexp = "^01(?:0|1|[6-9])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$", message = "전화번호 형식에 맞지 않습니다.")
     String contact;
 
     public Member convertToEntity() {
