f1638895ad40bcf22ccd6fc957c76be124d137d0
youhyeoneee <khukyh2018@khu.ac.kr>
2024-05-21 09:26:16 +0900

    feat: add id field and constructor

--- src/main/java/com/example/shoppingmall/member/Member.java
+++ src/main/java/com/example/shoppingmall/member/Member.java
@@ -8,13 +8,21 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class Member {
-    String userId;
-    String pw;
-    String name;
-    String email;
-    String contact;
+    private int id; // PK
+    private String userId;
+    private String pw;
+    private String name;
+    private String email;
+    private String contact;
+
+    public Member(String userId, String pw, String name, String email, String contact) {
+        this.userId = userId;
+        this.pw = pw;
+        this.name = name;
+        this.email = email;
+        this.contact = contact;
+    }
 
     public static Member fromDtoToEntity(MemberDTO memberDto) {
         return new Member(memberDto.getUserId(), memberDto.getPw(),
