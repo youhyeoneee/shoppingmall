01b3bc112ce565d9781e410c6cf7f668acc4d6fa
youhyeoneee <khukyh2018@khu.ac.kr>
2024-05-23 08:32:03 +0900

    feat: apply spring data jpa

--- src/main/java/com/example/shoppingmall/member/MemberRepository.java
+++ src/main/java/com/example/shoppingmall/member/MemberRepository.java
@@ -1,44 +1,20 @@
 package com.example.shoppingmall.member;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 
 @Repository
-public class MemberRepository {
+public interface MemberRepository extends CrudRepository<Member, Integer> {
 
-    @Autowired
-    @PersistenceContext
-    EntityManager entityManager;
+    <S extends Member> S save(S entity);
 
-    Map<String, Member> memberTable = new HashMap<>();
+    Optional<Member> findById(Integer id);
 
+    Optional<Member> findByUserId(String userId);
 
-    public void save(Member member) {
-        entityManager.persist(member);
-    }
-
-    public Member findByUserId(String userId) {
-        String jpql = "SELECT m FROM Member AS m WHERE m.userId = :userId";
-
-        return entityManager.createQuery(jpql, Member.class)
-                .setParameter("userId", userId)
-                .getSingleResult();
-    }
-
-    public Member findById(int id) {
-        return entityManager.find(Member.class, id);
-    }
-
-    public List<Member> findsByUserIdPw(@Param("userId") String userId, @Param("password") String password) {
-        List<Member> resultList = entityManager.createQuery("select m from Member as m where m.userId = :userId and m.pw = :password", Member.class).setParameter("userId", userId).setParameter("password", password).getResultList();
-        return resultList;
-    }
+    Optional<Member> findByUserIdAndPw(String userId, String pw);
 
 }
