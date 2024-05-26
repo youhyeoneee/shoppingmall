d69b58049a9768fe02a43dc873fcd183b07b377e
youhyeoneee <khukyh2018@khu.ac.kr>
2024-05-26 22:01:44 +0900

    refactor: use positive validator

--- src/main/java/com/example/shoppingmall/product/ProductDTO.java
+++ src/main/java/com/example/shoppingmall/product/ProductDTO.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,13 +16,13 @@ public class ProductDTO {
     @Pattern (regexp = "^[a-zA-Z0-0]*$", message = "상품 이름은 영어와 숫자만 가능합니다.")
     private String name;
 
-    @Min(value = 0, message = "가격은 0 이상의 정수여야 합니다.")
+    @Positive(message = "가격은 양수여야 합니다.")
     private int price;
 
     @NotBlank(message = "설명을 입력해주세요.")
     private String description;
 
-    @Min(value = 1, message = "카테고리 아이디는 1 이상의 정수여야 합니다.")
+    @Positive(message = "카테고리 아이디는 양수여야 합니다.")
     private int categoryId;
 
     public Product convertToEntity() {
